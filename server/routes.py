0d3296cc9747d01b0a08d5ae50761108ebb46abe
Wycliffe Munyes <wyclefmunyes@gmail.com>
2024-07-03 10:15:47 +0300

    Complete lab

---
+++ server/routes.py
@@ -0,0 +1,46 @@
+from flask import Flask, request, jsonify, abort
+from models import db, Restaurant, Pizza, RestaurantPizza
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+db.init_app(app)
+
+@app.route('/restaurants', methods=['GET'])
+def get_restaurants():
+    restaurants = Restaurant.query.all()
+    return jsonify([restaurant.to_dict() for restaurant in restaurants])
+
+@app.route('/restaurants/<int:id>', methods=['GET'])
+def get_restaurant(id):
+    restaurant = Restaurant.query.get_or_404(id)
+    return jsonify(restaurant.to_dict())
+
+@app.route('/restaurants/<int:id>', methods=['DELETE'])
+def delete_restaurant(id):
+    restaurant = Restaurant.query.get_or_404(id)
+    db.session.delete(restaurant)
+    db.session.commit()
+    return '', 204
+
+@app.route('/pizzas', methods=['GET'])
+def get_pizzas():
+    pizzas = Pizza.query.all()
+    return jsonify([pizza.to_dict() for pizza in pizzas])
+
+@app.route('/restaurant_pizzas', methods=['POST'])
+def create_restaurant_pizza():
+    data = request.get_json()
+    try:
+        restaurant_pizza = RestaurantPizza(
+            price=data['price'],
+            restaurant_id=data['restaurant_id'],
+            pizza_id=data['pizza_id']
+        )
+        db.session.add(restaurant_pizza)
+        db.session.commit()
+        return jsonify(restaurant_pizza.to_dict()), 201
+    except ValueError as e:
+        return jsonify({"errors": [str(e)]}), 422
+
+if __name__ == '__main__':
+    app.run(port=5555, debug=True)
